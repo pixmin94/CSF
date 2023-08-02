a27c4645e622814d850eeae51f0bed28a3bd4171
pixmin94 <pixmin@outlook.com>
2023-08-02 12:19:50 +0800

    WS1 initial commit

---
+++ day1/src/app/components/fruits/fruits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruits',
+  templateUrl: './fruits.component.html',
+  styleUrls: ['./fruits.component.css']
+})
+export class FruitsComponent {
+  fruit: string = 'Durian'
+  fullName: string = 'Fred Fred'
+}
