8aef429f02b573ddf6de07bf3cf597b1095cce25
pixmin94 <pixmin@outlook.com>
2023-08-25 17:02:45 +0800

    CWS7 frontend create order model and service

--- day7/src/app/app.module.ts
+++ day7/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateOrderComponent } from './components/create-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateOrderComponent
   ],
   imports: [
     BrowserModule,
---
+++ day7/src/app/order.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+import { Order } from "./models";
+import { firstValueFrom } from "rxjs";
+
+@Injectable()
+export class OrderService {
+  constructor (private http: HttpClient) { }
+
+  submitOrder(order: Order) {
+    //maybe need to convert to json??
+    return firstValueFrom(
+      this.http.post<any>('/api/order' , order)
+    )
+  }
+}
